b26a0dc21aa52848b2885d8f42e0c37e5ae00d10
mamun-prodhan <mamunpr9@gmail.com>
2023-09-11 23:31:32 +0600

    the dials is completed

--- src/TheDials/TheDials.jsx
+++ src/TheDials/TheDials.jsx
@@ -7,28 +7,32 @@ import tablet from "../../src/assets/tablet.png";
 
 const TheDials = () => {
   return (
-    <div className="px-10 lg:px-48 mt-32">
+    <div className="px-4 md:px-10 lg:px-48 mt-32">
       {/* the dials start */}
-      <div className="flex flex-col md:flex-row-reverse items-center justify-center">
+      <div className="flex gap-6 md:gap-0 flex-col md:flex-row-reverse items-center justify-center">
         <div className="flex-1">
-          <img className="w-[200px] lg:w-[295px]" src={dial} alt="" />
+          <img
+            className="w-[200px] md:w-[250px] lg:w-[295px]"
+            src={dial}
+            alt=""
+          />
         </div>
         <div className="flex-1">
-          <p className="text-2xl text-center font-semibold text-[#474A59]">
+          <p className="text-xl md:text-2xl lg:text-3xl text-center font-semibold text-[#474A59]">
             01/
           </p>
         </div>
         <div className="flex-1">
-          <h2 className="mb-4 text-2xl text-[#474A59] font-semibold uppercase">
+          <h2 className="mb-1.5 md:mb-2 lg:mb-4 text-lg md:text-xl lg:text-2xl text-[#474A59] font-semibold uppercase">
             the dials
           </h2>
-          <hr className="h-0.5 w-11 bg-[#E6E6E6]" />
-          <p className=" text-justify mt-10 mb-14 text-base font-normal text-[#474A59]">
+          <hr className="h-0.5 w-8 md:w-11 bg-[#E6E6E6]" />
+          <p className=" text-justify mt-4 md:mt-4 lg:mt-10 mb-6 md:mb-6 lg:mb-14 text-sm lg:text-base font-normal text-[#474A59]">
             Keep it classic with the sweeping dome bezel or add the fire and
             brilliance of a diamond packed bezel. Even Better, coordinate with a
             ring of the most vivid sapphire stones.
           </p>
-          <button className="px-10 py-3 border-solid border rounded-3xl border-[#474A59] text-base font-normal uppercase text-[#3D3B3C]">
+          <button className="px-4 md:px-5 lg:px-10 py-1.5 md:py-2 lg:py-3 border-solid border rounded-3xl border-[#474A59] text-sm lg:text-base font-normal uppercase text-[#3D3B3C]">
             Configure
           </button>
         </div>
