517e7c0a8c66b5bc17166ce4b17c476071dbfb48
mamun-prodhan <mamunpr9@gmail.com>
2023-09-11 23:39:35 +0600

    how it works section completed

--- src/TheDials/TheDials.jsx
+++ src/TheDials/TheDials.jsx
@@ -39,8 +39,64 @@ const TheDials = () => {
       </div>
       {/* the dials end */}
       {/* the bezels start */}
+      <div className="mt-16 md:mt-24 lg:mt-36 flex gap-6 md:gap-0 flex-col md:flex-row items-center justify-center">
+        <div className="flex-1">
+          <img
+            className="w-[200px] md:w-[250px] lg:w-[295px]"
+            src={bezels}
+            alt=""
+          />
+        </div>
+        <div className="flex-1">
+          <p className="text-xl md:text-2xl lg:text-3xl text-center font-semibold text-[#474A59]">
+            02/
+          </p>
+        </div>
+        <div className="flex-1">
+          <h2 className="mb-1.5 md:mb-2 lg:mb-4 text-lg md:text-xl lg:text-2xl text-[#474A59] font-semibold uppercase">
+            the bezels
+          </h2>
+          <hr className="h-0.5 w-8 md:w-11 bg-[#E6E6E6]" />
+          <p className=" text-justify mt-4 md:mt-4 lg:mt-10 mb-6 md:mb-6 lg:mb-14 text-sm lg:text-base font-normal text-[#474A59]">
+            Keep it classic with the sweeping dome bezel or add the fire and
+            brilliance of a diamond packed bezel. Even Better, coordinate with a
+            ring of the most vivid sapphire stones.
+          </p>
+          <button className="px-4 md:px-5 lg:px-10 py-1.5 md:py-2 lg:py-3 border-solid border rounded-3xl border-[#474A59] text-sm lg:text-base font-normal uppercase text-[#3D3B3C]">
+            Configure
+          </button>
+        </div>
+      </div>
       {/* the bezrls end */}
       {/* the straps start */}
+      <div className="mt-16 md:mt-24 lg:mt-36 flex gap-6 md:gap-0 flex-col md:flex-row-reverse items-center justify-center">
+        <div className="flex-1">
+          <img
+            className="w-[200px] md:w-[250px] lg:w-[295px]"
+            src={straps}
+            alt=""
+          />
+        </div>
+        <div className="flex-1">
+          <p className="text-xl md:text-2xl lg:text-3xl text-center font-semibold text-[#474A59]">
+            03/
+          </p>
+        </div>
+        <div className="flex-1">
+          <h2 className="mb-1.5 md:mb-2 lg:mb-4 text-lg md:text-xl lg:text-2xl text-[#474A59] font-semibold uppercase">
+            the straps
+          </h2>
+          <hr className="h-0.5 w-8 md:w-11 bg-[#E6E6E6]" />
+          <p className=" text-justify mt-4 md:mt-4 lg:mt-10 mb-6 md:mb-6 lg:mb-14 text-sm lg:text-base font-normal text-[#474A59]">
+            A choice between the distinctive design of the Jubilee or the sporty
+            Oyster bracelet made even harder by the assortment of our vivid
+            integrated rubber straps
+          </p>
+          <button className="px-4 md:px-5 lg:px-10 py-1.5 md:py-2 lg:py-3 border-solid border rounded-3xl border-[#474A59] text-sm lg:text-base font-normal uppercase text-[#3D3B3C]">
+            Configure
+          </button>
+        </div>
+      </div>
       {/* the straps end */}
       {/* tiempo*/}
       {/* <div className="mt-96 mb-32">
